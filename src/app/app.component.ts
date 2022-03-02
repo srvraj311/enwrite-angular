58b4f5e7f571a80d8393b27e75fd1a74d83730e9
srvraj311 <sourabhraj311@gmail.com>
2022-03-03 00:00:26 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'enWrite-angular';
+  loggedIn: boolean = this.isUserLoggedIn();
+  isUserLoggedIn(): boolean {
+    return true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  //{ path: '', component: null },
+  //{ path: 'about', component: null },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SplashScreenComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
